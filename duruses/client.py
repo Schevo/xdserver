cd6f271a94954b7a312d9e97281b1db9e66edec7
Matthew R. Scott <gldnspud@gmail.com>
2009-09-15 16:27:06 -0700

    Partially working implementation of client and server.

--- duruses/client.py
+++ duruses/client.py
@@ -2,9 +2,20 @@ import sys
 
 from argparse import ArgumentParser
 
-from durus.error import ProtocolError
-from durus.storage_server import SocketAddress
-from durus.utils import read, read_int4, write, write_all
+from durus.error import (
+    ConflictError, DurusKeyError, ProtocolError, ReadConflictError)
+from durus.serialize import split_oids
+from durus.storage import Storage
+from durus.storage_server import (
+    SocketAddress,
+    STATUS_OKAY, STATUS_KEYERROR, STATUS_INVALID,
+    )
+from durus.utils import (
+    iteritems,
+    as_bytes, join_bytes,
+    int4_to_str,
+    read, read_int4, write, write_all, write_int4, write_int4_str,
+    )
 
 from duruses.server import DEFAULT_HOST, DEFAULT_PORT, PROTOCOL
 
@@ -18,17 +29,19 @@ class Client(object):
         if self.server_protocol() != PROTOCOL:
             raise ProtocolError("Protocol version mismatch.")
 
+    # Server commands.
+
     def disconnect(self):
         if self.socket is not None:
             write(self.socket, '.')
             self.socket.close()
             self.socket = None
 
-    def enumerate_all(self):
+    def list_all(self):
         write(self.socket, 'A')
         return list(self._enumerate_database_names())
 
-    def enumerate_open(self):
+    def list_open(self):
         write(self.socket, 'E')
         return list(self._enumerate_database_names())
 
@@ -45,8 +58,149 @@ class Client(object):
         self.disconnect()
 
     def server_protocol(self):
-        write_all(self.socket, 'V')
+        write(self.socket, 'V')
         return read(self.socket, 4)
+
+    # Database commands.
+
+    def _write_database_name(self, db_name):
+        write_int4(self.socket, len(db_name))
+        write(self.socket, db_name)
+
+    def close(self, db_name):
+        write(self.socket, 'X')
+        self._write_database_name(db_name)
+
+    def destroy(self, db_name):
+        write(self.socket, 'D')
+        self._write_database_name(db_name)
+
+    def open(self, db_name):
+        write(self.socket, 'O')
+        self._write_database_name(db_name)
+
+    def storage(self, db_name):
+        self.open(db_name)
+        return ClientStorage(self, db_name)
+
+
+class ClientStorage(Storage):
+
+    def __init__(self, client, db_name):
+        self.client = client
+        self.db_name = int4_to_str(len(db_name)) + db_name
+        self.socket = client.socket
+        self.oid_pool = []
+        self.oid_pool_size = 32
+        self.begin()
+
+    def _get_load_response(self, oid):
+        status = read(self.socket, 1)
+        if status == STATUS_OKAY:
+            pass
+        elif status == STATUS_INVALID:
+            raise ReadConflictError([oid])
+        elif status == STATUS_KEYERROR:
+            raise DurusKeyError(oid)
+        else:
+            raise ProtocolError('status=%r, oid=%r' % (status, oid))
+        n = read_int4(self.socket)
+        record = read(self.socket, n)
+        return record
+
+    def _send_command(self, command):
+        write_all(self.socket, command, self.db_name)
+
+    def begin(self):
+        self.records = {}
+        self.transaction_new_oids = []
+
+    def bulk_load(self, oids):
+        oid_str = join_bytes(oids)
+        num_oids, remainder = divmod(len(oid_str), 8)
+        assert remainder == 0, remainder
+        self._send_command('B')
+        write_all(self.socket, int4_to_str(num_oids), oid_str)
+        records = [self._get_load_response(oid) for oid in oids]
+        for record in records:
+            yield record
+
+    def close(self):
+        # No-op on server side, but destroys this instance so that
+        # it conforms to API.
+        self.socket = None
+
+    def end(self, handle_invalidations=None):
+        self._send_command('C')
+        n = read_int4(self.socket)
+        oid_list = []
+        if n != 0:
+            packed_oids = read(self.socket, 8 * n)
+            oid_list = split_oids(packed_oids)
+            try:
+                handle_invalidations(oid_list)
+            except ConflictError:
+                self.transaction_new_oids.reverse()
+                self.oid_pool.extend(self.transaction_new_oids)
+                assert len(self.oid_pool) == len(set(self.oid_pool))
+                self.begin() # clear out records and transaction_new_oids
+                write_int4(self.socket, 0)
+                raise
+        tdata = []
+        for oid, record in iteritems(self.records):
+            tdata.append(int4_to_str(8 + len(record)))
+            tdata.append(as_bytes(oid))
+            tdata.append(record)
+        tdata = join_bytes(tdata)
+        write_int4_str(self.socket, tdata)
+        self.records.clear()
+        if len(tdata) > 0:
+            status = read(self.socket, 1)
+            if status == STATUS_OKAY:
+                pass
+            elif status == STATUS_INVALID:
+                raise WriteConflictError()
+            else:
+                raise ProtocolError(
+                    'server returned invalid status %r' % status)
+
+    def load(self, oid):
+        self._send_command('L')
+        write(self.socket, oid)
+        return self._get_load_response(oid)
+
+    def new_oid(self):
+        if not self.oid_pool:
+            batch = self.oid_pool_size
+            self._send_command('M')
+            write(self.socket, chr(batch))
+            self.oid_pool = split_oids(read(self.socket, 8 * batch))
+            self.oid_pool.reverse()
+            assert len(self.oid_pool) == len(set(self.oid_pool))
+        oid = self.oid_pool.pop()
+        assert oid not in self.oid_pool
+        self.transaction_new_oids.append(oid)
+        return oid
+
+    def pack(self):
+        self._send_command('P')
+        status = read(self.socket, 1)
+        if status != STATUS_OKAY:
+            raise ProtocolError('server returned invalid status %r' % status)
+
+    def store(self, oid, record):
+        assert len(oid) == 8
+        assert oid not in self.records
+        self.records[oid] = record
+
+    def sync(self):
+        self._send_command('S')
+        n = read_int4(self.socket)
+        if n == 0:
+            packed_oids = ''
+        else:
+            packed_oids = read(self.socket, 8 * n)
+        return split_oids(packed_oids)
 
 
 def main():
--- duruses/server.py
+++ duruses/server.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import sys
 
@@ -7,10 +8,19 @@ from cogen.core.coroutines import coro
 from cogen.core.schedulers import Scheduler
 from cogen.core.sockets import ConnectionClosed, Socket
 
-from durus.logger import log
+from durus.error import ConflictError
+from durus.logger import log, is_logging
 from durus.file_storage import FileStorage
-from durus.storage_server import DEFAULT_GCBYTES
-from durus.utils import int4_to_str
+from durus.serialize import extract_class_name, split_oids
+from durus.storage_server import (
+    DEFAULT_GCBYTES,
+    STATUS_OKAY, STATUS_KEYERROR, STATUS_INVALID,
+    ClientError,
+    )
+from durus.utils import (
+    int4_to_str, int8_to_str, str_to_int4, str_to_int8,
+    join_bytes,
+    )
 
 
 DEFAULT_HOST = '127.0.0.1'
@@ -18,13 +28,31 @@ DEFAULT_PORT = 22972
 
 PROTOCOL = int4_to_str(20001)
 
+EXTENSION = '.durus'
+
 
 def database_names(path):
     """Return a list of all Durus database names in a given path."""
     for filename in os.listdir(path):
         name, ext = os.path.splitext(filename)
-        if ext == '.durus':
+        if ext == EXTENSION:
             yield name
+
+
+class Client(object):
+
+    def __init__(self, client_socket):
+        f = self.f = client_socket.makefile()
+        self.invalid = {}
+        self.unused_oids = {}
+        self.read = f.read
+        self.write = f.write
+        self.flush = f.flush
+        self.close = f.close
+
+    @property
+    def closed(self):
+        return self.f.closed
 
 
 class Server(object):
@@ -52,13 +80,14 @@ class Server(object):
 
     def __init__(self, scheduler, path, storage_class=FileStorage,
                  host=DEFAULT_HOST, port=DEFAULT_PORT):
-        self.path = path
+        self.path = os.path.abspath(path)
         self.scheduler = scheduler
         self.storage_class = storage_class
         self.host = host
         self.port = port
         # Database name -> open storage mapping.  By default all are closed.
-        self.databases = dict((name, None) for name in database_names(path))
+        self.clients = set()
+        self.storages = {}
 
     @coro
     def dispatch(self):
@@ -77,7 +106,13 @@ class Server(object):
 
     @coro
     def serve_to_client(self, client_socket):
-        client = client_socket.makefile()
+        client = Client(client_socket)
+        # Initialize per-storage state for the new client.
+        client.invalid = dict(
+            (db_name, set()) for db_name in self.storages)
+        client.unused_oids = dict(
+            (db_name, set()) for db_name in self.storages)
+        self.clients.add(client)
         while not client.closed:
             try:
                 command = yield client.read(1)
@@ -89,29 +124,30 @@ class Server(object):
                     handler = getattr(self, handler_name)
                     yield handler(client)
                 elif command in self.db_handlers:
-                    handler_name = self.handlers[command]
+                    handler_name = self.db_handlers[command]
                     handler = getattr(self, handler_name)
-                    db_name = 'DUMMY' # XXX
+                    # Get database name.
+                    name_length = str_to_int4((yield client.read(4)))
+                    db_name = yield client.read(name_length)
                     yield handler(client, db_name)
+                yield client.flush()
         log(20, 'Connection closed.')
+        self.clients.remove(client)
 
     # Server handlers.
 
     @coro
     def handle_enumerate_all(self, client):
+        # A
         log(20, 'Enumerate all')
-        names = self.databases.keys()
+        names = list(database_names(self.path))
         yield self._handle_enumerate_database_names(client, names)
 
     @coro
     def handle_enumerate_open(self, client):
+        # E
         log(20, 'Enumerate open')
-        names = [
-            key
-            for key, value
-            in self.databases.iteritems()
-            if value is not None
-            ]
+        names = self.storages.keys()
         yield self._handle_enumerate_database_names(client, names)
 
     @coro
@@ -120,68 +156,250 @@ class Server(object):
         for name in names:
             yield client.write(int4_to_str(len(name)))
             yield client.write(name)
-        yield client.flush()
 
     @coro
     def handle_quit(self, client):
+        # Q
         log(20, 'Quit')
-        for storage in self.databases.values():
+        for storage in self.storages.values():
             if storage is not None:
                 storage.close()
         self.scheduler.stop()
 
     @coro
     def handle_version(self, client):
+        # V
         log(20, 'Version')
         yield client.write(PROTOCOL)
-        yield client.flush()
 
     @coro
     def handle_disconnect(self, client):
+        # .
         log(20, 'Disconnect')
         yield client.close()
 
     # Database handlers.
 
+    def _db_path(self, db_name):
+        db_path = os.path.join(self.path, db_name + EXTENSION)
+        db_path = os.path.abspath(db_path)
+        if not db_path.startswith(self.path):
+            raise RuntimeError('Malformed db name %s' % db_name)
+        return db_path
+
+    def _handle_invalidations(self, db_name, oids):
+        for c in self.clients:
+            c.invalid[db_name].update(oids)
+
+    def _new_oids(self, client, db_name, storage, count):
+        oids = []
+        while len(oids) < count:
+            oid = storage.new_oid()
+            for c in self.clients:
+                if oid in c.invalid[db_name]:
+                    oid = None
+                    break
+            if oid is not None:
+                oids.append(oid)
+        client.unused_oids[db_name].update(oids)
+        return oids
+
+    def _report_load_record(self, storage):
+        if storage.d_load_record and is_logging(5):
+            log(5, '\n'.join('%8s: %s' % (value, key)
+                             for key, value
+                             in sorted(storage.d_load_record.items())))
+            storage.d_load_record.clear()
+
+    @coro
+    def _send_load_response(self, client, db_name, storage, oid):
+        if oid in client.invalid[db_name]:
+            yield client.write(STATUS_INVALID)
+        else:
+            try:
+                record = storage.load(oid)
+            except KeyError:
+                log(10, 'KeyError %s', str_to_int8(oid))
+                yield client.write(STATUS_KEYERROR)
+            except ReadConflictError:
+                log(10, 'ReadConflictError %s', str_to_int8(oid))
+                yield client.write(STATUS_INVALID)
+            else:
+                if is_logging(5):
+                    class_name = extract_class_name(record)
+                    if class_name in storage.d_load_record:
+                        storage.d_load_record[class_name] += 1
+                    else:
+                        storage.d_load_record[class_name] = 1
+                    log(4, 'Load %-7s %s', str_to_int8(oid), class_name)
+                yield client.write(STATUS_OKAY)
+                yield client.write(int4_to_str(len(record)))
+                yield client.write(record)
+
+    def _sync_storage(self, db_name, storage):
+        self._handle_invalidations(db_name, storage.sync())
+
     @coro
     def handle_bulk_read(self, client, db_name):
-        pass
+        # B
+        log(20, 'Bulk read')
+        storage = self.storages[db_name]
+        number_of_oids = str_to_int4((yield client.read(4)))
+        oid_str_len = 8 * number_of_oids
+        oid_str = yield client.read(oid_str_len)
+        oids = split_oids(oid_str)
+        for oid in oids:
+            yield self._send_load_response(client, db_name, storage, oid)
 
     @coro
     def handle_commit(self, client, db_name):
-        pass
+        # C
+        log(20, 'Commit')
+        storage = self.storages[db_name]
+        self._sync_storage(db_name, storage)
+        invalid = client.invalid[db_name]
+        yield client.write(int4_to_str(len(invalid)))
+        yield client.write(join_bytes(invalid))
+        yield client.flush()
+        invalid.clear()
+        tdata_len = str_to_int4((yield client.read(4)))
+        if tdata_len == 0:
+            # Client decided not to commit (e.g. conflict)
+            return
+        tdata = yield client.read(tdata_len)
+        logging_debug = is_logging(10)
+        logging_debug and log(10, 'Committing %s bytes', tdata_len)
+        storage.begin()
+        i = 0
+        oids = []
+        while i < tdata_len:
+            rlen = str_to_int4(tdata[i:i+4])
+            i += 4
+            oid = tdata[i:i+8]
+            record = tdata[i+8:i+rlen]
+            i += rlen
+            if logging_debug:
+                class_name = extract_class_name(record)
+                log(10, '  oid=%-6s rlen=%-6s %s',
+                    str_to_int8(oid), rlen, class_name)
+            storage.store(oid, record)
+            oids.append(oid)
+        assert i == tdata_len
+        oid_set = set(oids)
+        for c in self.clients:
+            if c is not client:
+                if oid_set.intersection(c.unused_oids[db_name]):
+                    raise ClientError('invalid oid: %r' % oid)
+        try:
+            handle_invalidations = (
+                lambda oids: self._handle_invalidations(db_name, oids))
+            storage.end(handle_invalidations=handle_invalidations)
+        except ConflictError:
+            log(20, 'Conflict during commit')
+            yield client.write(STATUS_INVALID)
+        else:
+            self._report_load_record(storage)
+            log(20, 'Committed %3s objects %s bytes at %s',
+                len(oids), tdata_len, datetime.now())
+            yield client.write(STATUS_OKAY)
+            client.unused_oids[db_name] -= oid_set
+            for c in self.clients:
+                if c is not client:
+                    c.invalid[db_name].update(oids)
+            storage.d_bytes_since_pack += tdata_len + 8
 
     @coro
     def handle_destroy(self, client, db_name):
-        pass
+        # D
+        log(20, 'Destroy %s' % db_name)
+        if db_name in self.storages:
+            # Do nothing if it's still in use.
+            pass
+        else:
+            db_path = self._db_path(db_name)
+            os.unlink(db_path)
 
     @coro
     def handle_load(self, client, db_name):
-        pass
+        # L
+        log(20, 'Load')
+        storage = self.storages[db_name]
+        oid = yield client.read(8)
+        yield self._send_load_response(client, db_name, storage, oid)
 
     @coro
     def handle_new_oids(self, client, db_name):
-        pass
+        # M
+        log(20, 'New OIDs')
+        storage = self.storages[db_name]
+        count = ord((yield client.read(1)))
+        log(10, 'oids: %s', count)
+        yield client.write(
+            join_bytes(self._new_oids(client, db_name, storage, count)))
 
     @coro
     def handle_new_oid(self, client, db_name):
-        pass
+        # N
+        log(20, 'New OID')
+        storage = self.storages[db_name]
+        yield client.write(self._new_oids(client, db_name, storage, 1)[0])
 
     @coro
     def handle_open(self, client, db_name):
-        pass
+        # O
+        log(20, 'Open %s' % db_name)
+        if db_name not in self.storages:
+            db_path = self._db_path(db_name)
+            storage = self.storage_class(db_path)
+            storage.d_bytes_since_pack = 0
+            storage.d_load_record = {}
+            storage.d_packer = None
+            self.storages[db_name] = storage
+            # Initialize per-storage state for each client.
+            for c in self.clients:
+                c.invalid[db_name] = set()
+                c.unused_oids[db_name] = set()
 
     @coro
     def handle_pack(self, client, db_name):
-        pass
+        # P
+        log(20, 'Pack %s' % db_name)
+        storage = self.storages[db_name]
+        if stoage.d_packer is None:
+            log(20, 'Pack started at %s' % datetime.now())
+            storage.d_packer = storage.get_packer()
+            if storage.d_packer is None:
+                log(20, 'Cannot iteratively pack, performing full pack.')
+                storage.pack()
+                log(20, 'Pack completed at %s' % datetime.now())
+        else:
+            log(20, 'Pack already in progress at %s' % datetime.now())
+        yield client.write(STATUS_OKAY)
 
     @coro
     def handle_sync(self, client, db_name):
-        pass
+        # S
+        log(20, 'Sync %s' % db_name)
+        storage = self.storages[db_name]
+        self._report_load_record(storage)
+        self._sync_storage(db_name, storage)
+        invalid = client.invalid[db_name]
+        log(8, 'Sync %s', len(invalid))
+        yield client.write(int4_to_str(len(invalid)))
+        yield client.write(invalid)
+        invalid.clear()
 
     @coro
     def handle_close(self, client, db_name):
-        pass
+        # X
+        log(20, 'Close %s' % db_name)
+        if db_name in self.storages:
+            self.storages[db_name].close()
+            del self.storages[db_name]
+            # Remove per-storage state for each client.
+            for c in self.clients:
+                del c.invalid[db_name]
+                del c.unused_oids[db_name]
 
 
 def main():
